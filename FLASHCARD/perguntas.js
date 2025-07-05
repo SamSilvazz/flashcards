009dad6204eb30d46aadd8c89719e44ceb9fde7c
SamSilvazz <silva.machado.samuel@escola.pr.gov.br>
2025-07-05 12:09:53 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Teoria Musical',
+    ' O que é campo harmônico?',
+    ' É o conjunto de acordes que pertencem a uma mesma escala e tonalidade.')
+
+criaCartao(
+    'Prática Instrumental',
+    'Qual a diferença entre acorde com pestana e aberto?',
+    'Acordes com pestana são móveis e usam um dedo para pressionar várias cordas; abertos usam cordas soltas e têm posições fixas.'
+)
+
+criaCartao(
+    'História da Música',
+    'O que foi a Tropicália?',
+    ' Movimento musical dos anos 60 que misturou MPB com rock e influências estrangeiras.')
+
+criaCartao(
+    'Produção Musical',
+    'O que é mixagem?',
+    'É o processo de equilibrar os sons gravados para criar uma música bem ajustada e clara.')
